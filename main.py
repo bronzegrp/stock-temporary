731431924836ff644f83e1b16c33cbcb2bf60037
bronzegrp <bernabecg@institutobenaventeqro.edu.mx>
2026-06-15 12:49:34 -0600

    testing_api

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+api_key = 'CMAG9Q86Y425F8XY'
+stock_of_choice = 'TSLA'
+
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_of_choice}&apikey={api_key}'
+
+stock = requests.get(url=url)
+
+print(stock.status_code)
+
+print('\n')
+
+json_data = stock.json()
+print(json_data.keys())
+
+print(type(json_data['Meta Data']))
+print(type(json_data['Time Series (Daily)']))
+
+print(json_data['Meta Data'].keys())
+
+
+
+
